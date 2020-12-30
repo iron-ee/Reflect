4e2b3c474cd33426d56d7035da08f3e98a5c1dc1
irone <doyeck@naver.com>
2020-12-30 17:49:22 +0900

    리플렉션한 메서드의 파라메터 분석 및 값 주입하기

--- src/com/cos/reflect/controller/UserController.java
+++ src/com/cos/reflect/controller/UserController.java
@@ -1,18 +1,30 @@
 package com.cos.reflect.controller;
 
 import com.cos.reflect.anno.RequestMapping;
+import com.cos.reflect.controller.dto.JoinDto;
+import com.cos.reflect.controller.dto.LoginDto;
+import com.cos.reflect.model.User;
 
 public class UserController {
 	
-	@RequestMapping("/join")
-	public String join() {
+	@RequestMapping("/user/join")
+	public String join(JoinDto dto) {		// username, password, email
 		System.out.println("join() 함수 호출 됨");
+		System.out.println(dto);
 		return "/";
 	}
 	
-	@RequestMapping("/login")
-	public String login() {
+	@RequestMapping("/user/login")
+	public String login(LoginDto dto) {		// username, password
 		System.out.println("login() 함수 호출 됨");
+		System.out.println(dto);
+		return "/";
+	}
+	
+	@RequestMapping("/user/list")
+	public String list(User user) {		// username, password
+		System.out.println("list() 함수 호출 됨");
+		System.out.println(user);
 		return "/";
 	}
 	
--- src/com/cos/reflect/filter/Dispatcher.java
+++ src/com/cos/reflect/filter/Dispatcher.java
@@ -1,8 +1,11 @@
 package com.cos.reflect.filter;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Method;
+import java.lang.reflect.Parameter;
+import java.util.Enumeration;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
@@ -20,6 +23,8 @@ import com.cos.reflect.controller.UserController;
 // 분기 시키기
 public class Dispatcher implements Filter{
 
+	private boolean isMatching = false;
+	
 	@Override
 	public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain)
 			throws IOException, ServletException {
@@ -27,45 +32,33 @@ public class Dispatcher implements Filter{
 		HttpServletRequest request = (HttpServletRequest) req;
 		HttpServletResponse response = (HttpServletResponse) resp;
 		
-		//System.out.println("컨텍스트패스 : " + request.getContextPath()); // 프로젝트 시작주소
-		//System.out.println("식별자주소 : " + request.getRequestURI()); // 끝주소
-		//System.out.println("전체주소 : " + request.getRequestURL()); // 전체주소
-		
 		// /user 파싱하기
 		String endPoint = request.getRequestURI().replaceAll(request.getContextPath(),"");
 		System.out.println("엔드포인트 : " + endPoint);
 		
 		UserController userController = new UserController();
-//		if(endPoint.equals("/join")) {
-//			userController.join();
-//		}else if(endPoint.equals("/login")) {
-//			userController.login();
-//		}else if(endPoint.equals("/user")) {
-//			userController.user();
-//		}
+
 		
 		Method[] methods = userController.getClass().getDeclaredMethods();	// 그 파일에 메서드만 !!
-		for(Method method : methods) {
-			System.out.println();
-			if (endPoint.equals("/"+method.getName())) {
-				try {
-					method.invoke(userController);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		}
-		
+
 		for (Method method : methods) {
 			Annotation annotation = method.getDeclaredAnnotation(RequestMapping.class);
 			RequestMapping requestMapping = (RequestMapping)annotation;
-			System.out.println(requestMapping.value());
-			
+		
 			if (requestMapping.value().equals(endPoint)) {
+				isMatching = true;
 				try {
-//					method.invoke(userController);
-					String path = (String)method.invoke(userController);
-					RequestDispatcher dis = request.getRequestDispatcher(path);
+					Parameter[] params = method.getParameters();	// login(LoginDto dto)
+					String path = null;
+					if (params.length != 0) {
+						// 해당 dtoInstance를 리플렉션해서 set함수 호출(username, password)
+						Object dtoInstance = params[0].getType().newInstance();
+						setData(dtoInstance, request);
+						path = (String) method.invoke(userController, dtoInstance);
+					}else {
+						path = (String) method.invoke(userController);
+					}
+					RequestDispatcher dis = request.getRequestDispatcher(path);	// 필터를 다시 안 탐!!
 					dis.forward(request, response);
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -73,5 +66,45 @@ public class Dispatcher implements Filter{
 				break;
 			}
 		}
+		if (isMatching == false) {
+			response.setContentType("text/html; charset=utf-8");
+			PrintWriter out = response.getWriter();
+			out.println("잘못 된 주소 요청입니다. 404 error !");
+			out.flush();
+		}
+	}
+	
+	private <T> void setData(T instance, HttpServletRequest request) {
+		Enumeration<String> keys = request.getParameterNames();		// 크기 : 2 (username, password)
+		while (keys.hasMoreElements()) { 	// 크기 만큼 돈다 -> 크기 2 (username, password)면 2번 돈다.
+			String key = (String) keys.nextElement();
+			String methodKey = keyToMethodKey(key);	//setUsername
+			
+			Method[] methods = instance.getClass().getDeclaredMethods();	// 5개
+			
+			for(Method method : methods) {
+				if(method.getName().equals(methodKey)) {
+					try {
+						method.invoke(instance, request.getParameter(key));	//String 
+					} catch (Exception e) {
+						try {
+							int value = Integer.parseInt(request.getParameter(key));
+							method.invoke(instance, value);
+						} catch (Exception e2) {
+							e2.printStackTrace();
+						}
+						System.out.println("신경 쓸 필요없는 int 파싱 문제");
+					}
+				}
+			}
+		}
+	}
+	
+	private String keyToMethodKey(String key) {
+		String firstKey = "set";
+		String upperKey = key.substring(0,1).toUpperCase();
+		String remainKey = key.substring(1);
+		
+		return firstKey + upperKey + remainKey;
 	}
 }
